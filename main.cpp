d3618a5184f439e3781cdb6d1d87e947f9ce43c8
RuiMMC <rui.motacarneiro@heig-vd.ch>
2021-12-10 09:54:45 +0100

    Added test main
    Alignement corrected
    Correction of vectSum()
    Problem found in shuffleMatrix()

--- main.cpp
+++ main.cpp
@@ -23,54 +23,115 @@ using namespace std;
 int main() {
 
 
-    //-------------------------------------------------------------------------------------------
-    //  Initialisation of vectors and matrices
-    //-------------------------------------------------------------------------------------------
+   //-------------------------------------------------------------------------------------------
+   //  Initialisation of vectors and matrix
+   //-------------------------------------------------------------------------------------------
 
-    vect testVect1{3, 5, 9, 3, 1};   //sum = 21
-    vect testVect2{6, 5, 2, 4, 3, 3};//sum = 23
-    vect testVect3{5, 6, 3, 2, 5};   //sum = 21
-    vect testVect4{1, 5, 24, 7, 3};  //sum = 40
-    vect testVect5{};                //sum = 0
+   vect testVect1 = { 1, 3, 5, 2, 4};
+   vect testVect2 = {};
+   vect testVect3 = { 5, 4, 3, 2, 1};
 
-    matrix testMatrix1{testVect1, testVect3, testVect4, testVect1, testVect4};
-    matrix testMatrix2{testVect1, testVect3};
-    matrix testMatrix3{testVect2, testVect4};
-    matrix testMatrix4{};
-    matrix testMatrix5{testVect5, testVect5, testVect5};
-    matrix testMatrix6{testVect2, testVect3, testVect5};
+   matrix testMatrix1 = { {1, 2, 3, 4, 5},
+                        {2, 3, 4},
+                        {5, 3, 1, 4, 2}};
+   matrix testMatrix2 = { {1, 3, 5, 2, 3},
+                        {5, 4, 3, 4, 3},
+                        {7, 6, 5, 4, 3},
+                        {1, 2, 3, 2, 3},
+                        {7, 6, 7, 1, 2}};
+   matrix testMatrix3 = { {1, 2, 3},
+                        {4, 5, 6}};
+   matrix testMatrix4 = { {}, {}, {}};
+   matrix testMatrix5 = { {1, 2, 3},
+                        {},
+                        {4, 5, 6}};
 
+   //-------------------------------------------------------------------------------------------
+   //  Test functions
+   //-------------------------------------------------------------------------------------------
 
-    //-------------------------------------------------------------------------------------------
-    //  Test functions
-    //-------------------------------------------------------------------------------------------
+   //Display Vector
+   cout << "Vecteur 1 : " << testVect1 << endl;
+   cout << "Vecteur 2 : " << testVect2 << endl;
+   cout << "Vecteur 3 : " << testVect3 << endl;
+   cout << endl;
+   //Display Matrix
+   cout << "Matrice 1 : " << testMatrix1 << endl;
+   cout << "Matrice 2 : " << testMatrix2 << endl;
+   cout << "Matrice 3 : " << testMatrix3 << endl;
+   cout << "Matrice 4 : " << testMatrix4 << endl;
+   cout << "Matrice 5 : " << testMatrix5 << endl;
+   cout << endl;
+   //Square Matrix
+   cout << "Matrices carrees ? (Attendu = 01000) : "
+        << isSquare(testMatrix1) << isSquare(testMatrix2) << isSquare(testMatrix3)
+        << isSquare(testMatrix4) << isSquare(testMatrix5) << endl;
+   cout << endl;
+   //Regular Matrix
+   cout << "Matrices regulieres ? (Attendu = 01110) : "
+        << isRegular(testMatrix1) << isRegular(testMatrix2) << isRegular(testMatrix3)
+        << isRegular(testMatrix4) << isRegular(testMatrix5) << endl;
+   cout << endl;
+   //Minimum Column
+   cout << "Matrice 1 min col (Attendu = 3) : " << minCol(testMatrix1) << endl;
+   cout << "Matrice 2 min col (Attendu = 5) : " << minCol(testMatrix2) << endl;
+   cout << "Matrice 3 min col (Attendu = 3) : " << minCol(testMatrix3) << endl;
+   cout << "Matrice 4 min col (Attendu = 0) : " << minCol(testMatrix4) << endl;
+   cout << "Matrice 5 min col (Attendu = 0) : " << minCol(testMatrix5) << endl;
+   cout << endl;
+   //Sum Lines
+   cout << "Matrice 1 ligne (Attendu = 15,9,15)  : " << sumLine(testMatrix1) << endl;
+   cout << "Matrice 2 ligne (A = 14,19,25,11,23) : " << sumLine(testMatrix2) << endl;
+   cout << "Matrice 3 ligne (Attendu = 6,15)     : " << sumLine(testMatrix3) << endl;
+   cout << "Matrice 4 ligne (A = 0,0,0)          : " << sumLine(testMatrix4) << endl;
+   cout << "Matrice 5 ligne (A = 6,0,15)         : " << sumLine(testMatrix5) << endl;
+   cout << endl;
+   //Sum Columns
+   cout << "Matrice 1 colonne (A = 8,8,8,8,7)      : "
+        << sumColumn(testMatrix1) << endl;
+   cout << "Matrice 2 colonne (A = 21,21,23,13,14) : "
+        << sumColumn(testMatrix2) << endl;
+   cout << "Matrice 3 colonne (A = 5,7,9)          : "
+        << sumColumn(testMatrix3) << endl;
+   cout << "Matrice 4 colonne (A = rien ou 0)      : "
+        << sumColumn(testMatrix4) << endl;
+   cout << "Matrice 5 colonne (A = 5, 7, 9)        : "
+        << sumColumn(testMatrix5) << endl;
+   cout << endl;
+   //Vector with minimum Sum
+   cout << "Matrice 1 minimum (A = 2,3,4)     : " << vectSumMin(testMatrix1) << endl;
+   cout << "Matrice 2 minimum (A = 1,2,3,2,3) : " << vectSumMin(testMatrix2) << endl;
+   cout << "Matrice 3 minimum (A = 1,2,3)     : " << vectSumMin(testMatrix3) << endl;
+   cout << "Matrice 4 minimum (A = rien)      : " << vectSumMin(testMatrix4) << endl;
+   cout << "Matrice 5 minimum (A = rien)      : " << vectSumMin(testMatrix5) << endl;
+   cout << endl;
+   //Shuffle Matrix
+   shuffleMatrix(testMatrix1);
+   shuffleMatrix(testMatrix2);
+   shuffleMatrix(testMatrix3);
+   shuffleMatrix(testMatrix4);
+   shuffleMatrix(testMatrix5);
+   cout << "Matrice 1 melange : " << testMatrix1 << endl;
+   cout << "Matrice 2 melange : " << testMatrix2 << endl;
+   cout << "Matrice 3 melange : " << testMatrix3 << endl;
+   cout << "Matrice 4 melange : " << testMatrix4 << endl;
+   cout << "Matrice 5 melange : " << testMatrix5 << endl;
+   cout << endl;
+   //Sort Matrix
+   sortMatrix(testMatrix1);
+   sortMatrix(testMatrix2);
+   sortMatrix(testMatrix3);
+   sortMatrix(testMatrix4);
+   sortMatrix(testMatrix5);
+   cout << "Matrice 1 ordonnee : " << testMatrix1 << endl;
+   cout << "Matrice 2 ordonnee : " << testMatrix2 << endl;
+   cout << "Matrice 3 ordonnee : " << testMatrix3 << endl;
+   cout << "Matrice 4 ordonnee : " << testMatrix4 << endl;
+   cout << "Matrice 5 ordonnee : " << testMatrix5 << endl;
+   cout << endl;
 
-    // Square matrix
+   cout << "Appuyez sur ENTER pour continuer..." << endl;
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    // Regular matrix
-
-    // Not square/regular matrix
-
-    // empty matrix
-
-
-    cout << testMatrix2 << endl;
-    cout << isSquare(testMatrix2) << endl;
-
-    cout << testMatrix1 << endl;
-    cout << isSquare(testMatrix1) << endl;
-
-
-    shuffleMatrix(testMatrix1);
-    cout << testMatrix1 << endl;
-
-    cout << testMatrix6 << endl;
-    cout << minCol(testMatrix6) << endl;
-
-
-    cout << sumColumn(testMatrix6) << endl;
-
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
--- vmanip.cpp
+++ vmanip.cpp
@@ -27,31 +27,31 @@ using namespace std;
 //-------------------------------------------------------------------------------------------
 
 int vectSum(const vect &v) {
-    int i = 0;
-    accumulate(v.begin(), v.end(), i);
-    return i;
+   int i = 0;
+   i = accumulate(v.begin(), v.end(), i);
+   return i;
 }
 
 bool vectEqualSize(const vect &v1, const vect &v2) {
-    return v1.size() == v2.size();
+   return v1.size() == v2.size();
 }
 
 bool vectCompSizeLessThan(const vect &v1, const vect &v2) {
-    return v1.size() < v2.size();
+   return v1.size() < v2.size();
 }
 
 bool vectCompSizeGreaterThan(const vect &v1, const vect &v2) {
-    return v1.size() > v2.size();
+   return v1.size() > v2.size();
 }
 
 // Testing stuff for sumColumn function
 vect sumColumnFunction(vect &v1, const vect &v2) {
-    transform(v2.begin(), v2.end(), v1.begin(), v1.begin(), std::plus<>());
-    return v1;
+   transform(v2.begin(), v2.end(), v1.begin(), v1.begin(), std::plus<>());
+   return v1;
 }
 
 size_t maxCol(const matrix &m) {
-    return min_element(m.begin(), m.end(), vectCompSizeGreaterThan)->size();
+   return min_element(m.begin(), m.end(), vectCompSizeGreaterThan)->size();
 }
 
 //-------------------------------------------------------------------------------------------
@@ -59,65 +59,66 @@ size_t maxCol(const matrix &m) {
 //-------------------------------------------------------------------------------------------
 
 std::ostream &operator<<(std::ostream &os, const vect &v) {
-    cout << "(";
-    for (vect::const_iterator i = v.begin(); i != v.end(); ++i) {
-        cout << *i;
-        if (next(i) != v.end()) {
-            cout << ", ";
-        }
-    }
-    cout << ")";
+   cout << "(";
+   for (vect::const_iterator i = v.begin(); i != v.end(); ++i) {
+      cout << *i;
+      if (next(i) != v.end()) {
+         cout << ", ";
+      }
+   }
+   cout << ")";
 
-    return os;
+   return os;
 }
 
 ostream &operator<<(ostream &os, const matrix &m) {
-    //Start of Matrix
-    cout << "[";
-    for (matrix::const_iterator line = m.begin(); line != m.end(); ++line) {
-        cout << *line;
-        if (next(line) != m.end()) cout << ",";
-    }
-    //End of Matrix
-    cout << "]";
-    return os;
+   //Start of Matrix
+   cout << "[";
+   for (matrix::const_iterator line = m.begin(); line != m.end(); ++line) {
+      cout << *line;
+      if (next(line) != m.end()) cout << ",";
+   }
+   //End of Matrix
+   cout << "]";
+   return os;
 }
 
 bool isSquare(const matrix &m) {
-    if (m.empty()) return true;
-    return isRegular(m) && m[0].size() == m.size();
+   if (m.empty()) return true;
+   return isRegular(m) && m[0].size() == m.size();
 }
 
 bool isRegular(const matrix &m) {
-    if (m.empty()) return true;
-    return equal(m.begin(), m.end() - 1, m.begin() + 1, vectEqualSize);
+   if (m.empty()) return true;
+   return equal(m.begin(), m.end() - 1, m.begin() + 1, vectEqualSize);
 }
 
 size_t minCol(const matrix &m) {
-    return min_element(m.begin(), m.end(), vectCompSizeLessThan)->size();
+   return min_element(m.begin(), m.end(), vectCompSizeLessThan)->size();
 }
 
 vect sumLine(const matrix &m) {
-    vect sum(m.size());
-    transform(m.begin(), m.end(), sum.begin(), vectSum);
-    return sum;
+   vect sum(m.size());
+   transform(m.begin(), m.end(), sum.begin(), vectSum);
+   return sum;
 }
 
 vect sumColumn(const matrix &m) {
-//return accumulate(m.begin(),m.end(), ,sumColumnFunction)
+   //return accumulate(m.begin(),m.end(), ,sumColumnFunction)
+   return m[0];
 }
 
 vect vectSumMin(const matrix &m) {
-    vect sums = sumLine(m);
-    return m.at(distance(sums.begin(), min_element(sums.begin(), sums.end())));
+   vect sums = sumLine(m);
+   return m.at(size_t(distance(sums.begin(), min_element(sums.begin(), sums.end()))));
 }
 
 void shuffleMatrix(matrix &m) {
-    // Reference : https://www.cplusplus.com/reference/algorithm/shuffle/?kw=shuffle
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::shuffle(m.begin(), m.end(), std::default_random_engine(seed));
+   // Reference : https://www.cplusplus.com/reference/algorithm/shuffle/?kw=shuffle
+   // unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+   // std::shuffle(m.begin(), m.end(), std::default_random_engine(seed));
 }
 
 void sortMatrix(matrix &m) {
-    sort(m.begin(), m.end());
+   sort(m.begin(), m.end());
 }
